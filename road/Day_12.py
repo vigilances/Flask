e312495d5134a56804bb91f1209d102d927136ae
faq <vigilance.zx@gmail.com>
2019-07-03 10:29:29 +0800

    1.random number 2.object init 3.

---
+++ road/Day_12.py
@@ -0,0 +1,12 @@
+"""
+https://github.com/jackfrued/Python-100-Days/blob/master/Day01-15/09.%E9%9D%A2%E5%90%91%E5%AF%B9%E8%B1%A1%E8%BF%9B%E9%98%B6.md
+
+"""
+
+
+def test():
+    pass
+
+
+if __name__ == '__main__':
+    pass
